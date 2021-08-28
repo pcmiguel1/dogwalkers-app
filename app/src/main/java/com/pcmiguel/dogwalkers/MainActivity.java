31d95b2ab714d699db25858ba5b5e60bac623c50
Miguel <miguelpiresamaro@gmail.com>
2021-08-28 20:33:30 +0100

    fragments and navigation item selected

--- app/src/main/java/com/pcmiguel/dogwalkers/MainActivity.java
+++ app/src/main/java/com/pcmiguel/dogwalkers/MainActivity.java
@@ -1,17 +1,63 @@
 package com.pcmiguel.dogwalkers;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.MenuItem;
 import android.view.WindowManager;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView navigationView;
+    private Fragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        navigationView = findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.body_container, new HomeFragment()).commit();
+        navigationView.setSelectedItemId(R.id.nav_home);
+
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                fragment = null;
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        item.setChecked(true);
+                        fragment = new HomeFragment();
+                        break;
+
+                    case R.id.nav_favourites:
+                        item.setChecked(true);
+                        fragment = new FavouritesFragment();
+                        break;
+
+                    case R.id.nav_orderHistory:
+                        item.setChecked(true);
+                        fragment = new OrderHistoryFragment();
+                        break;
+
+                    case R.id.nav_profile:
+                        item.setChecked(true);
+                        fragment = new ProfileFragment();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.body_container, fragment).commit();
+
+                return false;
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/pcmiguel/dogwalkers/OrderHistoryFragment.java
@@ -0,0 +1,24 @@
+package com.pcmiguel.dogwalkers;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class OrderHistoryFragment extends Fragment {
+
+    private View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_order_history, container, false);
+
+
+        return view;
+    }
+}
